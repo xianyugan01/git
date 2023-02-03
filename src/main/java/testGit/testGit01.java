380b8688ba174e38ef9fade28c8e9ffefc3d9101
yugan <yugan@atguigu.com>
2023-02-03 22:06:13 +0800

    master01

--- src/main/java/testGit/testGit01.java
+++ src/main/java/testGit/testGit01.java
@@ -7,5 +7,6 @@ public class testGit01 {
         System.out.println("hello2");
         System.out.println("hello3");
         System.out.println("hello-hot-fix1");
+        System.out.println("master");
     }
 }
